dac95e41cd546d4500df12aa254adb4ee8b6bb44
uzunovd <duyz@ya.ru>
2019-02-25 00:41:41 +0400

    Отредактировал.

--- Lesson1-H-ex3.py
+++ Lesson1-H-ex3.py
@@ -1,11 +1,11 @@
 # Hard Задача-3
 n = int(input("Введите n:"))
 m = int(input("Введите m:"))
-i = 0
+i = 1
 
-while i < n:
+while i <= n:
     if i % 2 != 0:
-        print("BBBAAA" * m)
-    else:
         print("AAABBB" * m)
+    else:
+        print("BBBAAA" * m)
     i += 1
