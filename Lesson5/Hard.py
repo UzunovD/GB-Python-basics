2e428a85d75b834877791875de62dbd42fa98e31
uzunovd <duyz@ya.ru>
2019-03-12 05:03:36 +0400

    Вытаскивает и удаляет

--- Lesson5/Hard.py
+++ Lesson5/Hard.py
@@ -12,12 +12,17 @@ PATH = input("Введите полный путь к каталогу: ")
 
 dirs_path = []
 for d in os.listdir(PATH):
-    if os.path.isdir:
+    if os.path.isdir(os.path.join(PATH, d)):
         dirs_path.append(os.path.join(PATH, d))
-
 
 for di in dirs_path:
     os.chdir(di)
     for f in os.listdir(di):
         os.rename(os.path.join(di, f), os.path.join(os.path.split(di)[0], f))
 
+os.chdir(PATH)
+dirs = os.listdir(PATH)
+dirs_2 = dirs[:]
+for i in dirs_2:
+    if os.path.isdir(os.path.join(PATH, i)):
+        os.rmdir(i)
